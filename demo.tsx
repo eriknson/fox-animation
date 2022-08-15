77fbab955ff95507753c7e3331ee19125c6975f7
eriknson <eriks@mail.se>
2022-08-15 16:28:15 +0200

    dark inside color

--- demo.tsx
+++ demo.tsx
@@ -14,8 +14,8 @@ if (container) {
       hemisphereColor1={[1, 1, 1]}
       hemisphereColor0={[0.5, 0.5, 0.5]}
       fogColor={[1, 1, 1]}
-      interiorColor0={[0.8, 0.4, 0]}
-      interiorColor1={[0.9, 0.5, 0.1]}
+      interiorColor0={[0.1, 0.1, 0.1]}
+      interiorColor1={[0.3, 0.3, 0.3]}
       noGLFallback={<div>WebGL not supported :(</div>}
       enableZoom={false}
       followMouse={false}
